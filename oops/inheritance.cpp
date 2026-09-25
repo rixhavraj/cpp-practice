7fa96729a333c9189c81f04d31a82ac6820131fd
Rishav Raj <rixhavraj@gmail.com>
2026-09-25 23:10:12 +0530

    inheritance

---
+++ oops/inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+// Base class
+class Vehicle {
+public:
+    std::string brand = "Ford";
+    void honk() {
+        std::cout << "Tuut, tuut!\n";
+    }
+};
+
+// Derived class inheriting from Vehicle
+class Car : public Vehicle {
+public:
+    std::string model = "Mustang";
+};
+
+int main() {
+    Car myCar;
+    myCar.honk(); // Inherited method
+    std::cout << myCar.brand + " " + myCar.model; // Inherited and own attributes
+    return 0;
+}   
+
